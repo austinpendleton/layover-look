ee40f5ec2a545af22eac2d1b1156ad7834bb4e71
austinp <auspendleton@gmail.com>
2024-08-22 19:11:24 -0600

    App updated reviews

--- src/App.js
+++ src/App.js
@@ -1,1 +1,62 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import ReviewForm from "./components/ReviewForm";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ReviewList from "./components/ReviewList";
 
+const App = () => {
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios.defaults.headers.common["x-auth-token"] = token;
+    } else {
+      delete axios.defaults.headers.common["x-auth-token"];
+    }
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/submit-review"
+          element={
+            <ProtectedRoute>
+              <ReviewForm />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/reviews"
+          element={
+            <ProtectedRoute>
+              <ReviewList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/reviews/:id"
+          element={
+            <ProtectedRoute>
+              <ReviewDetail />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
