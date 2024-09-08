4744c103d4557291a369cd6a3598e0721fd25161
austinp <auspendleton@gmail.com>
2024-09-08 17:58:08 -0600

    Hide navbar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./blocks/App.css";
-import Dashboard from "./components/Dashboard";
-import ReviewForm from "./components/ReviewForm";
-import ReviewList from "./components/ReviewList";
-import ReviewDetail from "./components/ReviewDetail";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import NavBar from "./components/NavBar";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import LogIn from "./components/Login";
 import Register from "./components/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
+const AppContent = () => {
+  const location = useLocation();
+
+  const shouldHideNavBar =
+    location.pathname === "/login" || location.pathname === "/register";
+
   return (
-    <div className="main__content">
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/reviews" element={<ReviewList />} />
-          <Route path="/review/:id" element={<ReviewDetail />} />
-          <Route
-            path="/submit-review"
-            element={
-              <ProtectedRoute>
-                <ReviewForm />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Router>
+    <div className="App">
+      {!shouldHideNavBar && <NavBar />}
+      <Routes>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={<ProtectedRoute component={Dashboard} />}
+        />
+      </Routes>
     </div>
   );
-}
+};
+
+const App = () => (
+  <Router>
+    <AppContent />
+  </Router>
+);
 
 export default App;
