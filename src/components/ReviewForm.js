3a66d9406e27cd75a3c4c70d2f99ee43436a2052
austinp <auspendleton@gmail.com>
2024-09-11 16:37:44 -0600

    Review form JS update

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "../blocks/ReviewForm.css";
 
 const ReviewForm = () => {
   const [formData, setFormData] = useState({
