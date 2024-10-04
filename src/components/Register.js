fb58c052461ed50e887b903fb472ad7cb75a85a7
austinp <auspendleton@gmail.com>
2024-10-03 21:12:38 -0600

    Register page update

--- src/components/Register.js
+++ src/components/Register.js
@@ -44,57 +44,59 @@ const Register = () => {
   };
 
   return (
-    <div className="register-container">
-      <div className="logo-container">
-        <img src={logo} alt="Layover Look logo" className="register-logo" />
+    <div className="register-page">
+      <div className="register-container">
+        <div className="logo-container">
+          <img src={logo} alt="Layover Look logo" className="register-logo" />
+        </div>
+        <h1>Register</h1>
+        <form className="form" onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              minLength="6"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="password2"
+              value={password2}
+              onChange={onChange}
+              minLength="6"
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p>
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
       </div>
-      <h1>Register</h1>
-      <form className="form" onSubmit={onSubmit}>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            value={name}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="email"
-            placeholder="Email Address"
-            name="email"
-            value={email}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={password}
-            onChange={onChange}
-            minLength="6"
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="password2"
-            value={password2}
-            onChange={onChange}
-            minLength="6"
-          />
-        </div>
-        <input type="submit" className="btn btn-primary" value="Register" />
-      </form>
-      <p>
-        Already have an account? <Link to="/login">Sign In</Link>
-      </p>
     </div>
   );
 };
